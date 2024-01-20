d27ba6d59e04de5f9a8ad46a8d1bb5c976079aa2
Err IsNil <errisnil@gmail.com>
2024-01-20 14:47:19 +0530

    x11 stuff

---
+++ g15/ArchLinux/fonts.sh
@@ -0,0 +1,9 @@
+# Install paru and alias yay
+yay -S ttf-nerd-fonts-symbols ttf-fira-code noto-fonts
+yay -S ttf-amiri otf-crimson
+yay -S noto-fonts-cjk
+yay -S ttf-hannom
+yay -S ttf-paratype
+yay -S ttf-sbl-biblit
+yay -S ttf-indic-otf
+yay -S lohit-fonts ttf-gujrati-fonts ttf-kannada-font ttf-lklug
